c36c302c9787507a21afcd52898d0bf4d5a0953f
rmb2020 <30849275+rmb2020@users.noreply.github.com>
2024-05-30 17:42:49 -0700

    AWS cognito signup, signin, forgetpwd, signout

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigation from './navigation';
+import config from './aws-exports';
+import Amplify from '@aws-amplify/core';
+
+Amplify.configure(config);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AppNavigation />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
