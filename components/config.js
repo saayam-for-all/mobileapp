f25909d08a410821fb262d116bd04d5b0c3e4cad
Parth <pbarot297@gmail.com>
2024-09-05 03:04:16 -0700

    Revert "merge conflict resolution"
    This reverts commit d14a8af0ad4f56c161d1e7991c13c5c28edebf83, reversing
    changes made to 4aef737d6c32d123044f18b2f11a48d51096d4e0.

--- components/config.js
+++
@@ -1,9 +0,0 @@
-import { Dimensions } from "react-native";
-
-
-const config = {
-    deviceWidth:Dimensions.get('window').width,
-    deviceHeight:Dimensions.get('window').height
-}
-
-export default config
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,10 +4,6 @@ import Home from '../screens/Home';
 import Profile from '../screens/Profile';
 import UserRequest from '../screens/UserRequest';
 import EditProfile from '../screens/EditProfile';
-import MyReqs from '../screens/AllRequests/MyReqs';
-import OtherRequests from '../screens/AllRequests/OtherRequests';
-import ManagedReqs from '../screens/AllRequests/ManagedReqs';
-
 
 const AppStack = createStackNavigator();
 
@@ -24,13 +20,6 @@ export default function App({ signOut }) {
         {() => <EditProfile signOut={signOut} />}
       </AppStack.Screen>
       <AppStack.Screen name="UserRequest" component={UserRequest} />
-      <AppStack.Screen name="MyReqs" component={MyReqs} 
-        options={{ title: 'My Requests' }}/>
-    <AppStack.Screen name="OtherRequests" component={OtherRequests} 
-        options={{ title: 'Other Requests' }}/>
-    <AppStack.Screen name="ManagedReqs" component={ManagedReqs} 
-    options={{ title: 'Managed Requests' }}/>
-      
     </AppStack.Navigator>
     
   );
