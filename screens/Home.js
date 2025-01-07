2ea47f07e3aedc101d43fc0fc86da83bbba4b1a5
Owen Hall <owen9hall@gmail.com>
2025-01-06 16:36:10 -0800

    Added administration button and placeholder administration page

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity, Linking, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity, Linking, SafeAreaView, Touchable } from 'react-native';
 import Button from '../components/Button';
 //import { Button } from '@react-native-material/core';
 import Icon from '@expo/vector-icons/Ionicons';
@@ -130,6 +130,9 @@ const styles = StyleSheet.create({
     color: '#4f8ef7',
     fontWeight: 'bold',
   },
+  adminButton: {
+    marginRight: -40,
+  },
 });
  
 export default function Home({ signOut }) {
@@ -175,10 +178,15 @@ export default function Home({ signOut }) {
       <View style={styles.topBar}>
       
         <Image source={require('../assets/saayamforall.jpeg')} style={styles.logo}/>
-        
+
+        <TouchableOpacity onPress={() => {navigation.navigate("Administration")}}>
+          <Ionicons name="build-outline" size={33} color="black" style={styles.adminButton}/>
+        </TouchableOpacity>
+
         <TouchableOpacity onPress={() => {navigation.navigate("Profile")}}>
            <Ionicons name="person-circle-outline" size={40} color="black" />
         </TouchableOpacity>
+        
         </View>
       
       {/* Search Bar */}
@@ -255,7 +263,7 @@ export default function Home({ signOut }) {
               return <Icon name={iconName} size={size} color={color} />;
             },
             tabBarActiveTintColor: '#4285F4',
-            tabBarInactiveTintColor: 'gray',
+            tabBarInactiveTintColor: 'grey',
             tabBarStyle: {
               height: 60,
               paddingBottom: 15,
