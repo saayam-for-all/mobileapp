439e510606f0a79fd0b744ebbc91ce9398e6429b
AakashReddy <aakashreddy3702@gmail.com>
2025-12-01 20:00:04 -0600

    Corrected Welcome page header UI

--- components/Header.js
+++ components/Header.js
@@ -9,34 +9,34 @@ import {
 import Button from '../components/Button';
 import config from '../components/config'
 
-const LogoImage ="https://saayamforall.org/wp-content/uploads/2023/03/saayamforall.jpeg";
+const LogoImage = "https://saayamforall.org/wp-content/uploads/2023/03/saayamforall.jpeg";
 
 export default function Header() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-      
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+
         <Image source={require('../assets/saayamforall.jpeg')}
-          style={{width: 50, height: 50, alignItems: 'left'}} />
-        </View>
-        <View style={{marginLeft:config.deviceWidth/1.6, alignItems: 'right'}}>
-          <Button
-          // onPress={() => navigation.navigate('Donate')}>
-          onPress={() => {Linking.openURL('https://www.paypal.com/donate/?hosted_button_id=4KLWNM5JWKJ4S')}}>
-            Donate
-          </Button>
-          </View>
+          style={{ width: 50, height: 50, alignItems: 'left' }} />
       </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-        flex: .17,
-        flexDirection: 'row',
-        backgroundColor: '#d1f0ef',
-        alignItems: 'center',
-        marginBottom: 10,   
-        marginTop: Platform.OS === "android" ? 20 : 0,    
-      },
-  });
+      <View style={{ marginLeft: config.deviceWidth / 1.6, alignItems: 'right' }}>
+        <Button
+          // onPress={() => navigation.navigate('Donate')}>
+          onPress={() => { Linking.openURL('https://www.paypal.com/donate/?hosted_button_id=4KLWNM5JWKJ4S') }}>
+          Donate
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    backgroundColor: '#d1f0ef',
+    alignItems: 'center',
+    paddingVertical: 3, // instead of flex
+    paddingHorizontal: 16,
+    marginTop: Platform.OS === "android" ? 20 : 0,
+  },
+});
