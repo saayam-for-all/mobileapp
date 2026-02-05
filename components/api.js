06e9d855111f03f127e45ddb418aa52e152c6f9b
Shenghan0329 <sc8326@nyu.edu>
2026-02-04 20:59:22 -0500

    Update latest endpoints

--- components/api.js
+++ components/api.js
@@ -1,66 +1,80 @@
 import axios from "axios";
 import { fetchAuthSession, getCurrentUser, signOut } from "aws-amplify/auth";
 
-const api = axios.create(
-    {
-        baseURL: process.env.EXPO_PUBLIC_API_URL,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      
-    });
-  
-   // Request interceptor
-  api.interceptors.request.use(
-    async (config) => {    
-      // Modify the request config (e.g., add headers) 
-        // config.headers.Authorization = 'Bearer YOUR_TOKEN';
-        const session = await fetchAuthSession();
-        //let token = session.tokens?.idToken?.toString();
-        //console.log ('token api', token)
-        idTokenExpire = session.tokens?.idToken?.payload?.exp;
-        refreshToken = session.tokens?.refreshToken;
-        currentTimeSeconds = Math.round(+new Date() / 1000);
-        if (idTokenExpire < currentTimeSeconds) {
-          getCurrentUser()
-            .then(async (res) => {
-              try {
-                const refreshedSession = await fetchAuthSession({ forceRefresh: true });
-                config.headers.Authorization = refreshedSession.tokens?.idToken?.toString();
-                //console.log('Token refreshed')
-                return config;
-              } catch (err) {
-                await signOut();
-              }
-            });
-        } else {
-          config.headers.Authorization = session.tokens?.idToken?.toString();
-          //console.log('no refresh', idTokenExpire + ' ' + currentTimeSeconds)
-          return config;
-        }    
+const api = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-      /*  if (token) {
-          config.headers['Authorization'] =  token; 
-         // console.log('header', config.headers.Authorization)
-        }
-     return config;*/
-    },
-    (error) => {
-      // Handle request errors
-      return Promise.reject(error);
+const getToken = async () => {
+  try {
+    const session = await fetchAuthSession();
+    return session?.tokens?.idToken?.toString();
+  } catch (error) {
+    console.log("Error fetching token:", error);
+    return null;
+  }
+};
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await getToken();
+    if (token) {
+      config.headers.Authorization = `${token}`;
     }
-    
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
 
-//Response interceptor
+let isRefreshing = false;
+let failedRequestqueue = [];
+
 api.interceptors.response.use(
-  (response) => {
-    //Modify the response data
-    // response.data = transformData(response.data);
-    return response;
-  },
-  (error) => {
-    // Handle response errors
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedRequestqueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers.Authorization = `${token}`;
+            return api.request(originalRequest);
+          })
+          .catch((err) => Promise.reject(error));
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+      try {
+        const newToken = await getToken();
+        if (!newToken) {
+          console.log("Error refreshing token. Logging Out..");
+          signOut();
+          return Promise.reject(error);
+        }
+        failedRequestqueue.forEach((p) => p.resolve(newToken));
+        failedRequestqueue = [];
+        originalRequest.headers.Authorization = `${newToken}`;
+        return api.request(originalRequest);
+      } catch (refreshError) {
+        console.log("Token refresh failed:", refreshError);
+        failedRequestqueue.forEach((p) => p.reject(refreshError));
+        failedRequestqueue = [];
+        signOut();
+        return Promise.reject(error);
+      } finally {
+        isRefreshing = false;
+      }
+    }
     return Promise.reject(error);
   },
 );
