c6d58ac317fb372ceaeb07e2bc451826f6f4b15d
Shenghan Cheng <sc8326@nyu.edu>
2026-05-28 13:19:09 -0400

    Integrate API for help requests

--- screens/AllRequests/ManagedReqs.js
+++ screens/AllRequests/ManagedReqs.js
@@ -1,13 +1,8 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, Linking, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
-
-import { useNavigation } from '@react-navigation/native';
-
-import { MyReqData } from '../../data/MyReqData';
-
-//import config from '../../components/config'
 import AllRequests from '../../components/AllRequests';
+import { getManagedRequests } from '../../services/requestServices';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,23 +10,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    //marginTop: 20,
   },
- 
 });
- 
+
 export default function ManagedReqs() {
-  const [userName, setUserName] = useState('');
-  const navigation = useNavigation();
- 
-  return (    
-    <SafeAreaView style={styles.container}>
-              
-  
-           
-      <AllRequests data={MyReqData}/>      
-            
-  
-    </SafeAreaView>
-  )
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await getManagedRequests();
+      setData(res["body"]);
+    } catch (error) {
+      console.log('data error', error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (data && data.length) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <AllRequests data={data} />
+      </SafeAreaView>
+    );
+  }
 }
--- screens/AllRequests/OtherRequests.js
+++ screens/AllRequests/OtherRequests.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 
-import { useNavigation } from '@react-navigation/native';
-import { MyReqData } from '../../data/MyReqData';
 import AllRequests from '../../components/AllRequests';
+import { getOthersRequests } from '../../services/requestServices';
 
 
 const styles = StyleSheet.create({
@@ -12,21 +11,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    //marginTop: 20,
-  },  
+  },
 });
- 
+
 export default function OtherRequests() {
-  const [userName, setUserName] = useState('');
-  const navigation = useNavigation();
- 
-  return (    
-    <SafeAreaView style={styles.container}>            
-     
-           
-      <AllRequests data={MyReqData}/>      
-            
-    
-    </SafeAreaView>
-  )
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await getOthersRequests();
+      setData(res["body"]);
+    } catch (error) {
+      console.log('data error', error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (data && data.length) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <AllRequests data={data} />
+      </SafeAreaView>
+    );
+  }
 }
