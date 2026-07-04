e971b6451d4f24952564bed9cd5fd7319678b19d
moddebaixa <moddebaixa@gmail.com>
2026-07-04 13:38:52 +0200

    adjustment for test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
 
 
   moduleNameMapper: {
-  '^aws-amplify/auth$': '<rootDir>/components/__mocks__/awsAmplifyAuth.js',
+  '^aws-amplify/auth$': '<rootDir>/components/__mocks__/awsAmplifyMock.js',
 },
 
   transformIgnorePatterns: [
