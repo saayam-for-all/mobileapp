2b2b6cbfeac9d89174b9fa554819de504b531c78
moddebaixa <moddebaixa@gmail.com>
2026-07-04 12:45:07 +0200

    adjustment for test

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  preset: 'react-native',
-
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|expo(nent)?|@expo|expo-file-system|react-native-country-codes-picker|@react-navigation)/)',
-  ],
-
-  setupFilesAfterEnv: [
-    '@testing-library/jest-native/extend-expect',
-  ],
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
